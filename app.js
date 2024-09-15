83af4fd8c17552e70c04356e3d19d0796ab6cead
ababyb <assmaa.bouselham@gmail.com>
2024-09-15 21:50:17 +0100

    fixed

--- app.js
+++ app.js
@@ -3,13 +3,22 @@ document.addEventListener("DOMContentLoaded", ()  => {
     const images = document.querySelectorAll ("img");
 
     for (const image of images){
-        fetch("https://dog.ceo/api/breeds/image/random")
+        fetch("https://images.unsplash.com/photo-1502174832274-bc176e52765a?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aWNlJTIwY3JlYW18ZW58MHx8MHx8fDA%3D")
         .then(response  => response.json())
         .then (data  => {
             image.src = data.message
-            image.width = 100;
-            image.height = 100;
+            image.width = 50;
+            image.height = 30;
         })
     }
 
+for (const image of images){
+    fetch("https://images.unsplash.com/photo-1580915411954-282cb1b0d780?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fGljZSUyMGNyZWFtfGVufDB8fDB8fHww")
+    .then(response  => response.json())
+    .then (data  => {
+        image.src = data.message
+        image.width = 50;
+        image.height = 30;
+    })
+}
 })
